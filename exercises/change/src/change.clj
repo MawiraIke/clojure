8e0617dfd71aae82a5fe686bca8d45b4122cb52a
MawiraIke <mawiraike@gmail.com>
2019-10-23 10:49:51 +0300

    Challenge completed
    Signed-off-by: MawiraIkemawiraike@gmail.com <mawiraike@gmail.com>

--- exercises/change/src/change.clj
+++ exercises/change/src/change.clj
@@ -6,7 +6,26 @@
   (vec (for [i (range co)]
          1)))
 
-(defn choose-mod [num]
+(defn sel [vx y]
+  "Returns a vector containing the key and the value
+  of the highest value in changes which is less than vx."
+  (cond
+    (>= vx (first y))
+    [:100 100]
+
+    (>= vx (second y))
+    [:25 25]
+
+    (>= vx (nth y 2))
+    [:10 10]
+
+    (>= vx (nth y 3))
+    [:5 5]
+
+    (>= vx (last y))
+    [:1 1]))
+
+(defn issue [num]
   (let [x (count (gen-range num))
         y (reverse changes)]
     (loop [vx x
@@ -15,22 +34,7 @@
                 :10  0
                 :5   0
                 :1   0}]
-      (cond
-        (> vx (first y))
-        (recur (- vx 100) (assoc acc :100 (inc (get acc :100))))
-
-        (> vx (second y))
-        (print ">25")
-
-        (> vx (get y 3))
-        (print ">10")
-
-        (> vx (get y 4))
-        (print ">5")
-
-        (> vx (last y))
-        (print ">1")
-        ))))
-
-(defn issue []
-  ())
+      (if (> vx 0)
+        (let [[k v] (sel vx y)]
+          (recur (- vx v) (assoc acc k (inc (k acc)))))
+        acc))))
