ab36444cd76babdb5d969f7d748294b3e71d72e8
MawiraIke <mawiraike@gmail.com>
2019-10-25 21:04:27 +0300

    Challenge completed
    Signed-off-by: MawiraIkemawiraike@gmail.com <mawiraike@gmail.com>

--- exercises/clock/src/clock.clj
+++ exercises/clock/src/clock.clj
@@ -1,10 +1,26 @@
 (ns clock)
 
-(defn clock->string [clock]
-)
+(defn time* [hours minutes]
+  {:hours hours :minutes minutes})
 
 (defn clock [hours minutes]
-)
+  {:hours hours :minutes minutes})
+
+(defn clock->string [clock]
+  (str (:hours clock) " hours, " (:minutes clock) " minutes."))
+
+(defn to-min [time]
+  (let [hours (:hours time)
+        min (:minutes time)]
+    {:hours 0 :minutes (+ (* hours 60) min)}))
+
+(defn to-hours [time]
+  (let [hours (int (/ time 60))
+        minutes (mod time 60)]
+    {:hours hours :minutes minutes}))
+
+(defn add-logic [clock time]
+  (to-hours (+ (:minutes (to-min clock)) (:minutes (to-min time)))))
 
 (defn add-time [clock time]
-)
+  (clock->string (to-hours (+ (:minutes (to-min clock)) (:minutes (to-min time))))))
