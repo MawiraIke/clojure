7be687be58415d10eca41fe0d8adc3eb110a5c28
MawiraIke <mawiraike@gmail.com>
2019-10-28 13:17:22 +0300

    Challenge complete
    Signed-off-by: MawiraIkemawiraike@gmail.com <mawiraike@gmail.com>

--- exercises/diamond/src/diamond.clj
+++ exercises/diamond/src/diamond.clj
@@ -1,21 +1,31 @@
 (ns diamond
   (:require [clojure.string :as str]))
 
-(def alphabet "abcdefghijklmnopqrstuvwxyz")
-
 (def alphabet2 (map char (range (int \A) (inc (int \Z)))))
 
-(defn perform-print [item n limit]
-  (println (str "*" (str/upper-case item) "*")))
+(defn- pad [x]
+  (apply str (repeat x "*")))
+
+(defn- one-row [letter [inner-padding outer-padding]]
+  (if (= inner-padding 0)
+    (str (pad outer-padding) letter (pad outer-padding))
+    (str (pad outer-padding) letter (pad inner-padding) letter (pad outer-padding))))
+
+(defn- row-details [num-letters]
+  (let [inner-dots (conj (iterate (partial + 2) 1) 0)
+        outer-dots (iterate dec (dec num-letters))]
+    (take num-letters (map vector inner-dots outer-dots))))
 
 (defn diamond [letter]
   "Loops over the letters of the alphabet until the letter (argument) is got. "
-  (let [num-letters (- (int letter) (dec (int \A)))]
-    (loop [idx 0]
-      (if (= (str (nth alphabet idx)) letter)
-        (do
-          (perform-print (nth alphabet idx) idx num-letters)
-          "Done")
-        (do
-          (perform-print (nth alphabet idx) idx num-letters)
-          (recur (inc idx)))))))
+  (let [num-letters (- (int letter) (dec (int \A)))
+        top-half (map one-row (take num-letters alphabet2) (row-details num-letters))]
+    ;(loop [idx 0]
+    ;  (if (= (str (nth alphabet idx)) (str letter))
+    ;    (do
+    ;      (perform-print (nth alphabet idx) idx num-letters)
+    ;      "Done")
+    ;    (do
+    ;      (perform-print (nth alphabet idx) idx num-letters)
+    ;      (recur (inc idx)))))
+    (concat top-half (rest (reverse top-half)))))
