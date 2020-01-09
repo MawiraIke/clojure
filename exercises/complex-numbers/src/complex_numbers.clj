267ce34a7a85a67b1df50098943b7d9156ff9c55
MawiraIke <mawiraike@gmail.com>
2020-01-09 22:08:44 +0300

    Complex numbers

--- exercises/complex-numbers/src/complex_numbers.clj
+++ exercises/complex-numbers/src/complex_numbers.clj
@@ -1,33 +1,51 @@
-(ns complex-numbers)
+(ns complex-numbers
+  (:import (clojure.lang PersistentVector)))
 
-(defn real [[a b]] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn pow [base exp]
+  (if (zero? base)
+    0
+    (loop [base base, exp exp, acc 1]
+      (if (zero? exp)
+        acc
+        (recur base (dec exp) (* acc base))))))
 
-(defn imaginary [[a b]] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defprotocol Complex
+  (complex+ [complex value])
+  (complex- [complex value])
+  (complex* [complex value])
+  (complex-div [complex value]))
 
-(defn abs [[a b]] ;; <- arglist goes here
-  ;; your code goes here
-)
+(extend-type PersistentVector
+  Complex
+  (complex+ [[a i b] [c i-2 d]]
+    [(+ a c) (* (+ b d) i)])
+  (complex- [[a i b] [c i-2 d]]
+    [(- a c) (* (- b d) i)])
+  (complex* [[a i b] [c i-2 d]]
+    [(* a (- c (* b d)))
+     (* (* b (+ c (* a d))) i)])
+  (complex-div [[a i b] [c i-2 d]]
+    [(/ (* a (+ c (* b d))) (+ (pow c 2) (pow d 2)))
+     (/ (* b (- c (* a d))) (* (+ (pow c 2) (pow d 2)) i))]))
 
-(defn conjugate [[a b]] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn do-func
+  [f [a b e] [c d g]]
+  (if (nil? e)
+    (f [a 1 b] [c 1 d])
+    (f [a b e] [c d g])))
 
-(defn add [[a b] [c d]] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn real [[a b]] a)
 
-(defn sub [[a b] [c d]] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn imaginary [[a b]] b)
 
-(defn mul [[a b] [c d]] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn abs [[a b]])
 
-(defn div [[a b] [c d]] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn conjugate [[a b]] (- a b))
+
+(defn add [cur val] (do-func complex+ cur val))
+
+(defn sub [cur val] (do-func complex- cur val))
+
+(defn mul [cur val] (do-func complex* cur val))
+
+(defn div [cur val] (do-func complex-div cur val))
